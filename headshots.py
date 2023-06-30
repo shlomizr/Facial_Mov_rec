25851bfa069d6acd9f90aea98197e66e1ba02bab
shlomizr <shlomizr71@gmail.com>
2023-06-30 02:48:46 -0400

    Uploaded headshot script

--- headshots.py
+++ headshots.py
@@ -1,33 +1,44 @@
+#! /usr/bin/python
+
 import cv2
 
-name = 'Shlomi' #replace with your name
+name = 'Shlomi'  # Replace with your name
 
-cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(1)  # Use index 0 for the default camera
 
-cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("press space to take a photo", 500, 300)
+cv2.namedWindow("Capture Images", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Capture Images", 500, 300)
 
 img_counter = 0
+capture_mode = False
 
 while True:
     ret, frame = cam.read()
     if not ret:
-        print("failed to grab frame")
+        print("Failed to grab frame")
         break
-    cv2.imshow("press space to take a photo", frame)
+
+    if capture_mode:
+        cv2.putText(frame, "Press SPACE to capture (ESC to exit)", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    else:
+        cv2.putText(frame, "Press C to start capturing (ESC to exit)", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+    cv2.imshow("Capture Images", frame)
 
     k = cv2.waitKey(1)
-    if k%256 == 27:
+    if k == 27:
         # ESC pressed
         print("Escape hit, closing...")
         break
-    elif k%256 == 32:
+    elif k == ord('c') or k == ord('C'):
+        # C pressed
+        capture_mode = not capture_mode
+    elif k == 32 and capture_mode:
         # SPACE pressed
-        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+        img_name = "dataset/" + name + "/image_{}.jpg".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
 
 cam.release()
-
 cv2.destroyAllWindows()
