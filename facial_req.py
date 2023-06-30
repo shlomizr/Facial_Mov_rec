9dd101f67841330110ded236b86ee713c4bf184b
shlomizr <shlomizr71@gmail.com>
2023-06-30 05:51:47 -0400

    Updated files

--- facial_req.py
+++ facial_req.py
@@ -1,107 +1,84 @@
-#! /usr/bin/python
-
-# import the necessary packages
-from imutils.video import VideoStream
-from imutils.video import FPS
-import face_recognition
+import cv2
 import imutils
 import pickle
 import time
-import cv2
+import face_recognition
+from imutils.video import VideoStream
+from imutils.video import FPS
 
-#Initialize 'currentname' to trigger only when a new person is identified.
-currentname = "unknown"
-#Determine faces from encodings.pickle file model created from train_model.py
-encodingsP = "encodings.pickle"
+print("[INFO] Starting facial recognition...")
 
-# load the known faces and embeddings along with OpenCV's Haar
-# cascade for face detection
-print("[INFO] loading encodings + face detector...")
-data = pickle.loads(open(encodingsP, "rb").read())
+encodings_file = "encodings.pickle"
 
-# initialize the video stream and allow the camera sensor to warm up
-# Set the ser to the followng
-# src = 0 : for the build in single web cam, could be your laptop webcam
-# src = 2 : I had to set it to 2 inorder to use the USB webcam attached to my laptop
-vs = VideoStream(src=1,framerate=10).start()
-#vs = VideoStream(usePiCamera=True).start()
+# Load the known faces and encodings
+print("[INFO] Loading encodings...")
+data = pickle.loads(open(encodings_file, "rb").read())
+
+# Initialize video stream and allow the camera to warm up
+vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
-# start the FPS counter
+# Initialize the FPS counter
 fps = FPS().start()
 
-# loop over frames from the video file stream
+# Main loop for video stream processing
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to 500px (to speedup processing)
-	frame = vs.read()
-	frame = imutils.resize(frame, width=500)
-	# Detect the fce boxes
-	boxes = face_recognition.face_locations(frame)
-	# compute the facial embeddings for each face bounding box
-	encodings = face_recognition.face_encodings(frame, boxes)
-	names = []
+    # Read a frame from the video stream and resize it
+    frame = vs.read()
+    frame = imutils.resize(frame, width=500)
 
-	# loop over the facial embeddings
-	for encoding in encodings:
-		# attempt to match each face in the input image to our known
-		# encodings
-		matches = face_recognition.compare_faces(data["encodings"],
-			encoding)
-		name = "Unknown" #if face is not recognized, then print Unknown
+    # Detect face locations in the frame
+    boxes = face_recognition.face_locations(frame)
 
-		# check to see if we have found a match
-		if True in matches:
-			# find the indexes of all matched faces then initialize a
-			# dictionary to count the total number of times each face
-			# was matched
-			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			counts = {}
+    # Compute facial encodings for each face
+    encodings = face_recognition.face_encodings(frame, boxes)
 
-			# loop over the matched indexes and maintain a count for
-			# each recognized face face
-			for i in matchedIdxs:
-				name = data["names"][i]
-				counts[name] = counts.get(name, 0) + 1
+    # Initialize a list of names for recognized faces
+    names = []
 
-			# determine the recognized face with the largest number
-			# of votes (note: in the event of an unlikely tie Python
-			# will select first entry in the dictionary)
-			name = max(counts, key=counts.get)
+    # Loop over the facial encodings
+    for encoding in encodings:
+        # Attempt to match each face in the frame with known encodings
+        matches = face_recognition.compare_faces(data["encodings"], encoding)
+        name = "Unknown"
 
-			#If someone in your dataset is identified, print their name on the screen
-			if currentname != name:
-				currentname = name
-				print(currentname)
+        # Check if there is a match
+        if True in matches:
+            # Find the indexes of all matched faces
+            matched_idxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
 
-		# update the list of names
-		names.append(name)
+            # Loop over the matched indexes and count the votes for each recognized face
+            for i in matched_idxs:
+                name = data["names"][i]
+                counts[name] = counts.get(name, 0) + 1
 
-	# loop over the recognized faces
-	for ((top, right, bottom, left), name) in zip(boxes, names):
-		# draw the predicted face name on the image - color is in BGR
-		cv2.rectangle(frame, (left, top), (right, bottom),
-			(0, 255, 225), 2)
-		y = top - 15 if top - 15 > 15 else top + 15
-		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-			.8, (0, 255, 255), 2)
+            # Determine the recognized face with the highest vote count
+            name = max(counts, key=counts.get)
 
-	# display the image to our screen
-	cv2.imshow("Facial Recognition is Running", frame)
-	key = cv2.waitKey(1) & 0xFF
+        # Add the recognized name to the list
+        names.append(name)
 
-	# quit when 'q' key is pressed
-	if key == ord("q"):
-		break
+    # Loop over the recognized faces and draw bounding boxes and names
+    for ((top, right, bottom, left), name) in zip(boxes, names):
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-	# update the FPS counter
-	fps.update()
+    # Display the frame
+    cv2.imshow("Facial Recognition", frame)
 
-# stop the timer and display FPS information
+    # Check for 'q' key press to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Update the FPS counter
+    fps.update()
+
+# Stop the FPS counter and display information
 fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+print("[INFO] Elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] Approximate FPS: {:.2f}".format(fps.fps()))
 
-# do a bit of cleanup
+# Cleanup
 cv2.destroyAllWindows()
 vs.stop()
